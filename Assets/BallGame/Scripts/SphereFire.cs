19e85a8fc0a985d19b34b39d2ff752372c3285db
Tia Lim <limqiying19@gmail.com>
2019-11-06 00:33:15 -0500

    add ball game scripts

---
+++ Assets/BallGame/Scripts/SphereFire.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereFire : MonoBehaviour
+{
+    Rigidbody rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    public void ThrowBall(float x, float y, float z)
+    {
+        rb.AddForce(x, y, z);
+    }
+}
---
+++ Assets/BallGame/Scripts/SphereParentController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class SphereParentController : MonoBehaviour
+{
+    public GameObject spherePrefab;
+    public SphereFire script;
+    ARSessionOrigin m_SessionOrigin;
+    private Transform oTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        oTransform = transform;
+        m_SessionOrigin = GetComponent<ARSessionOrigin>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            GameObject spawnedSphere = Instantiate(spherePrefab);
+            Vector3 pos = new Vector3(oTransform.position.x, oTransform.position.y - 0.5f, oTransform.position.z + 1.0f);
+            m_SessionOrigin.MakeContentAppearAt(spawnedSphere.transform, pos, Quaternion.identity);
+            SphereFire fire = spawnedSphere.GetComponent<SphereFire>();
+            fire.ThrowBall(0.0f, 0.0f, 1.0f);
+        }
+    }
+}
